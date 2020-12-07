061c71f998924224f133e0c6c54ba74b64d840cf
hanch <jurco10510@gmail.com>
2020-12-08 00:21:30 +0100

    addet projectListViewModel for Scrum

--- src/view/ScrumMaster/ScrumMasterViewController.java
+++ src/view/ScrumMaster/ScrumMasterViewController.java
@@ -1,9 +1,12 @@
 package view.ScrumMaster;
 
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import model.ProjectListModel;
+import view.ProjectListViewModel;
+import view.ProjectViewModel;
 import view.ViewHandler;
 
 import java.awt.*;
@@ -11,11 +14,15 @@ import java.awt.*;
 public class ScrumMasterViewController {
 
 
-    public TextField searchProject;
-    public TableColumn projectList;
+    public TableView<ProjectViewModel> ProjectList;
+    public TableColumn<ProjectViewModel, String> title;
+    public TableColumn<ProjectViewModel, String> startDate;
+    public TableColumn<ProjectViewModel, String> deadLine;
+    public TableColumn<ProjectViewModel, String> state;
+
     private Region root;
     private ViewHandler viewHandler;
-    private ProjectListModel model;
+    private ProjectListViewModel smodel;
 
 
     public void BackOnClick()
@@ -28,8 +35,19 @@ public class ScrumMasterViewController {
     }
 
     public void init(Region root, ViewHandler viewHandler, ProjectListModel model) {
-        this.model = model;
+        this.smodel = new ProjectListViewModel(model);
         this.root = root;
         this.viewHandler = viewHandler;
+
+        title.setCellValueFactory(cellData -> cellData.getValue().getProjectTitle());
+        startDate.setCellValueFactory(cellData -> cellData.getValue().getProjectStartDate());
+        deadLine.setCellValueFactory(cellData -> cellData.getValue().getProjectDeadLine());
+        state.setCellValueFactory(cellData -> cellData.getValue().getProjectState());
+
+        ProjectList.setItems(smodel.getList());
+    }
+    public void reset()
+    {
+        smodel.update();
     }
 }
